5525ddad9b27526616b61b76ec2fc30937b068d4
Amal Benny <57700315+amalbenny7@users.noreply.github.com>
2025-03-23 14:45:06 +0000

    php test

--- assets/php/submit_form.php
+++ assets/php/submit_form.php
@@ -1,23 +1,22 @@
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = strip_tags(trim($_POST["name"]));
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     $message = trim($_POST["message"]);
 
-    if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        // Set a 400 (bad request) response code and exit.
+    // Validate input fields
+    if (empty($name) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Set a 400 (bad request) response code and exit
         http_response_code(400);
         echo "Oops! There was a problem with your submission. Please complete the form and try again.";
         exit;
     }
 
-    // Recipient email
+    // Setup email recipient and subject
     $recipient = "amalbenny.contact@gmail.com";
-
-    // Email subject
     $subject = "New contact from $name";
 
-    // Email content
+    // Build the email content
     $email_content = "Name: $name\n";
     $email_content .= "Email: $email\n\n";
     $email_content .= "Message:\n$message\n";
@@ -27,17 +26,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Sending email
     if (mail($recipient, $subject, $email_content, $email_headers)) {
-        // Set a 200 (okay) response code.
+        // Set a 200 (okay) response code
         http_response_code(200);
         echo "Thank You! Your message has been sent.";
     } else {
-        // Set a 500 (internal server error) response code.
+        // If mail fails, set a 500 internal server error response code
         http_response_code(500);
-        echo "Oops! Something went wrong and we couldn't send your message.";
+        echo "Oops! Something went wrong, and we couldn't send your message.";
     }
-
 } else {
-    // Not a POST request, set a 403 (forbidden) response code.
+    // If not a POST request, set a 403 (forbidden) response code
     http_response_code(403);
     echo "There was a problem with your submission, please try again.";
 }
